30b1efdbda745697dcfd933548614243444f773a
bHannah <borsikaaa1@gmail.com>
2024-04-19 09:21:24 +0200

    navigáció, keresőmező, legördülő menü, arucikkek lista adatai

---
+++ arucikk.js
@@ -0,0 +1,44 @@
+export const arucikkLISTA = [
+    {
+        nev: "arucikk",
+        kep: "kep.png",
+        kategoria: "kategoria",
+        leiras: 0,
+        ar: 10
+    },
+    {
+        nev: "",
+        kep: "",
+        kategoria: "",
+        leiras: "",
+        ar: ""
+    },
+    {
+        nev: "",
+        kep: "",
+        kategoria: "",
+        leiras: "",
+        ar: ""
+    },
+    {
+        nev: "",
+        kep: "",
+        kategoria: "",
+        leiras: "",
+        ar: ""
+    },
+    {
+        nev: "",
+        kep: "",
+        kategoria: "",
+        leiras: "",
+        ar: ""
+    },
+    {
+        nev: "",
+        kep: "",
+        kategoria: "",
+        leiras: "",
+        ar: ""
+    },
+]
---
+++ index.js
@@ -0,0 +1,1 @@
+import { arucikkLISTA } from "./arucikk.js" 
